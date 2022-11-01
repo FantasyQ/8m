640a7c20da5764cdaf450e0fe7b30ea740f4fde2
MartinChu <hunter77613@hotmail.com>
2022-11-01 23:54:09 +0800

    feat: fetch city and weather data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,115 @@
+import { useState, useMemo, useEffect } from 'react';
+import styled from 'styled-components';
+import _ from 'lodash';
+import { getCityList, getWeatherByCoordinates } from './api/openWeather';
+
+const StyledInputText = styled.input`
+  width: 200px;
+  padding: 5px 10px;
+  font-size: 24px;
+`
+const StyledDropDownUl = styled.ul`
+  width: 200px;
+  margin: 0;
+  padding: 0;
+  border: 1px solid #ccc;
+  li {
+    padding: 5px 10px;
+    list-style: none;
+    cursor: initial;
+    user-select: none;
+    &:hover {
+      background-color: #faa;
+    }
+  }
+`
+
+function InputDropDownMenu() {
+  const debounceSetState = useMemo(() => {
+    return _.debounce((inputValue, setFunc) => {
+      setFunc(inputValue);
+    }, 1000)
+  }, [])
+  const [needToFetch, setNeedToFetch] = useState(true)
+
+  // 查詢城市 - Start
+  const [cityNameInput, setCityNameInput] = useState('')
+  const [cityNameToFetch, setCityNameToFetch] = useState('')
+  const handleChange = (event) => {
+    setCityNameInput(event.target.value)
+    debounceSetState(event.target.value, setCityNameToFetch)
+  }
+  const [cityList, setCityList] = useState([])
+  useEffect(() => {
+    const fetchCityList = () => {
+      getCityList({
+        cityName: cityNameToFetch,
+      }).then((response) => {
+        setCityList(response.data)
+      })
+    }
+    if (needToFetch && cityNameToFetch !== '') {
+      fetchCityList()
+    }
+  }, [needToFetch, cityNameToFetch])
+  // 查詢城市 - End
+
+  // 查詢城市天氣 - Start
+  const [weatherData, setWeatherData] = useState({})
+  const [citySelected, setCitySelected] = useState({})
+  const onCityBeSelected = (item) => {
+    const fetchWeatherData = () => {
+      getWeatherByCoordinates(item).then((response) => {
+        setWeatherData(response.data)
+      })
+    }
+    setCitySelected(item)
+    setCityNameInput(item.name)
+    setCityList([])
+    setNeedToFetch(false)
+    fetchWeatherData()
+  }
+  // 查詢城市天氣 - End
+
+  return (
+    <div>
+      <StyledInputText
+        type="text"
+        value={cityNameInput}
+        onChange={(event) => handleChange(event)}
+        onKeyDown={() => setNeedToFetch(true)}
+      />
+      {cityList.length > 0 && (
+        <StyledDropDownUl>
+          <>
+            {cityList.map((item) => (
+              <li
+                key={`${item.country}${item.state}${item.name}`}
+                onClick={() => onCityBeSelected(item)}
+              >
+                {item.name}, {item.state}, {item.country}
+              </li>
+            ))}
+          </>
+        </StyledDropDownUl>
+      )}
+      <h1>查詢城市：{citySelected.name}</h1>
+      <h2>{weatherData.timezone}</h2>
+    </div>
+  )
+}
+
 function App() {
+  const [weatherData, setWeatherData] = useState({})
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          天氣預報
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <InputDropDownMenu />
     </div>
   );
 }
